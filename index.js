eced689d176eae87896f79b38041611ea8630ec9
ambakhtiar <ambakhtiar88@gmail.com>
2025-07-30 02:15:01 +0600

    'almost'

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // Middleware 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: [
+        'http://localhost:5173',
+        'https://job-portal-d70bf.web.app',
+        'https://job-portal-d70bf.firebaseapp.com'
+    ],
     credentials: true
 }));
 app.use(express.json());
@@ -21,7 +25,7 @@ const logger = (req, res, next) => {
 }
 
 const verifyToken = (req, res, next) => {
-    const token = req?.cookies?.token;
+    const token = req.cookies?.token;
     console.log('Token ', token);
 
     if (!token) {
@@ -53,26 +57,35 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
         const jobCollection = client.db('jobPortal').collection('jobs');
         const jobApplicationCollection = client.db('jobPortal').collection('job_application');
 
-        // Auth related api 
+        // Auth related APIs
         app.post('/jwt', async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.JET_SECRET, { expiresIn: '6h' });
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: false // http://localhost:5000/signIn 
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             }).send({ success: true });
         })
 
-        // Job realted api 
+        app.post('/logout', (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            }).send({ success: true })
+        })
+
+        // Job realted APIs 
         app.get('/jobs', async (req, res) => {
             const email = req.query.email;
             let query = {};
